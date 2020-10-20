d44c8f8375d54e5f9a4df226247e2f0bfd335450
DonBusto <jorge.elbusto@opendeusto.es>
2020-10-20 10:00:23 +0200

    pasado del otro repo

---
+++ flask/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+#Mirar SQLAlchemy ORM
+
+
+# def login():
+#     error =None
+#     if request.method == 'POST':
+#         if valid_login(request.form['username'], request.form['password']):
+#             return log_the_user_in(request.form['username'])
+#         else:
+#             error = 'Invalid username or password'
+#     return render_template('login.html', error=error)
+if __name__ == "__main__":
+    app.run(debug=True)
