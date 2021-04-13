384675f384e7283a218206ec4208736dad759422
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2021-04-13 22:25:05 +0200

    with appcontext para crear db

--- base.py
+++ base.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from flask.cli import with_appcontext
 import os
 
 engine = create_engine(os.environ.get('DATABASE_URL'))
@@ -9,5 +10,7 @@ Session = sessionmaker(bind=engine)
 Base = declarative_base()
 Base.metadata.create_all(engine)
 
+@click.command()
+@with_appcontext
 def init_db():
     Base.metadata.create_all(bind=engine)
