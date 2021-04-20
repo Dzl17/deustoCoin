f46f2e3b5c416a7cd7b043b8c13d62c341f963f0
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2021-04-20 23:01:24 +0200

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,33 +1,12 @@
 var cacheName = 'DeustoCoin';
 var filesToCache = [
-  './templates/accion.html',
-  './templates/accionalumnos.html',
-  './templates/adminacciones.html',
-  './templates/admincampanyas.html',
-  './templates/adminofertas.html',
-  './templates/base.html',
-  './templates/campanyas.html',
-  './templates/editoraccion.html',
-  './templates/editorcamp.html',
-  './templates/editoroferta.html',
-  './templates/empresas.html',
-  './templates/error.html',
-  './templates/historialtrans.html',
-  './templates/index.html',
-  './templates/ofertas.html',
-  './templates/pago.html',
-  './templates/recompensa.html',
-  './templates/register.html',
-  './templates/sobre.html',
-  './templates/subirimagen.html',
-  './templates/tab1cartera.html',
+  './',
   './static/css/main.css',
   './static/css/floating-labels.css',
   './static/css/sidebar.css',
   './static/js/sidebar.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -36,7 +15,6 @@ self.addEventListener('install', function(e) {
   );
 });
 
-/* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
   e.respondWith(
     caches.match(e.request).then(function(response) {
