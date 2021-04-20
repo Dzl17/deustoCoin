138df9b4ea0cc133e48471c95933bc200581cbcf
Jorge El Busto <33113637+bu5to@users.noreply.github.com>
2021-04-20 10:45:54 +0200

    config para pwa

--- config - copia.py
+++ config - copia.py
@@ -7,17 +7,16 @@ class Config(object):
     GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
     PRIVATE_KEY = "e49aed1a79c5f2c703b5651dd09c840d3193175fd748fbea37e00ce8d83a3c7d"
     TEST_ADDRESS = "0x99AD62313b591405Ba1C51aa50294245A36F1289"
-    DATABASE_URL = "postgres://guowqhhnreqdng:40feea3efe620ef6d243b65ab5f7cf4416646658f3b1eb51fcfe7e925c2d2b00@ec2-54-216-185-51.eu-west-1.compute.amazonaws.com:5432/datu2lta68lhk7"
+    DATABASE_URL = "postgresql://postgres:root@localhost:5432/deustoCoin"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PRIVATE_KEY = "e49aed1a79c5f2c703b5651dd09c840d3193175fd748fbea37e00ce8d83a3c7d"
     DEBUG = True
     IPFS_CONNECT_URL = "/dns/ipfs.infura.io/tcp/5001/https"
     IPFS_FILE_URL = "http://ipfs.infura.io:5001/ipfs/"
-    DOMAIN = "http://deustocoin.herokuapp.com"
+    DOMAIN = "http://localhost:5000"
     LANGUAGES = {
         'es': 'Spanish',
-        'en': 'English',
-        'eu': 'Basque'
+        'en': 'English'
     }
     PHRASEAPP_ENABLED = True
     PHRASEAPP_PREFIX = '{{__'
