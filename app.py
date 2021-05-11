59b1b8a68a265110fb21cb322101612adf59c770
Busto <jorge.elbusto@opendeusto.es>
2021-05-11 11:43:40 +0200

    try customchain

--- app.py
+++ app.py
@@ -81,8 +81,7 @@ def sendCoins(dest, amount, imgHash, urlProof):
         'value': web3.toWei(float_amount, 'ether'),
         'gas': 21000,
         'gasPrice': web3.toWei(50, 'gwei'),
-        'accion': accion.nombre,
-        'hashIpfs': imgHash
+        'customChain': {'accion': accion.nombre,'hashIpfs': imgHash}
     }
     signed_tx = web3.eth.account.signTransaction(tx, private_key)
     tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
