631b888af897543c5071d366ada83359da6b0e4c
Busto <jorge.elbusto@opendeusto.es>
2021-05-11 11:53:43 +0200

    try msg

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def sendCoins(dest, amount, imgHash, urlProof):
         'value': web3.toWei(float_amount, 'ether'),
         'gas': 21000,
         'gasPrice': web3.toWei(50, 'gwei'),
-        'data': {'accion': accion.nombre,'hashIpfs': imgHash}
+        'message': "Acción:" + accion.nombre + ", Hash de la imagen: " + imgHash
     }
     signed_tx = web3.eth.account.signTransaction(tx, private_key)
     tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
